604ebdd387086fe3c0d119b8c380316c987c6822
PedroVMota <pedromvmota@outlook.pt>
2023-03-05 15:56:09 +0000

    Makefile

--- Project-submit-00/ft_strrchr.c
+++ Project-submit-00/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: pvital-m <pvital-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 11:04:46 by pvital-m          #+#    #+#             */
-/*   Updated: 2023/03/05 15:55:23 by pvital-m         ###   ########.fr       */
+/*   Updated: 2023/03/05 15:56:06 by pvital-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int	len;
 
-	len = ft_strlen(s);
+	len = ft_strlen((char *)s);
 	while (len >= 0)
 	{
 		if (s[len] == c)
